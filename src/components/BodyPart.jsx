3e56b136205bd6a84810ad527d2c9d4110a73703
emilkaminski1022 <o.daugherty9se@gmail.com>
2022-06-18 15:38:44 +0430

    add onClick to BodyPart component

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -17,10 +17,21 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
       cursor: "pointer",
       gap: "47px",
     }}
-  
+    onClick={() => {
+      setBodyPart(item);
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+    }}
   >
     <img src={Icon} alt="dumbbell" style={{ width: "40px", height: "40px" }} />
-  
+    <Typography
+      fontSize="24px"
+      fontWeight="bold"
+      fontFamily="Alegreya"
+      color="#3A1212"
+      textTransform="capitalize"
+    >
+      {item}
+    </Typography>
   </Stack>
 );
 
