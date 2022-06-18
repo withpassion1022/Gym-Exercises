87d3644a245e0ad48deac77ea66942fb397c2ec1
emilkaminski1022 <o.daugherty9se@gmail.com>
2022-06-18 15:21:09 +0430

    add style to BodyPart component

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -1,11 +1,27 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+    sx={{
+      borderTop: bodyPart === item ? "4px solid #FF2625" : "",
+      background: "#fff",
+      borderBottomLeftRadius: "20px",
+      width: "270px",
+      height: "280px",
+      cursor: "pointer",
+      gap: "47px",
+    }}
+  
+  >
+    <img src={Icon} alt="dumbbell" style={{ width: "40px", height: "40px" }} />
+  
+  </Stack>
+);
 
-import React from 'react'
-
-const BodyPart = () => {
-  return (
-    <div>BodyPart</div>
-  )
-}
-
-export default BodyPart
+export default BodyPart;
