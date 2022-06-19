9a6354bed29f4074de13e2ecdfdab06ba113e99a
emilkaminski1022 <o.daugherty9se@gmail.com>
2022-06-19 10:30:19 +0430

    add style to Exercises comp

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
+        Showing Results
+      </Typography>
 
-export default Exercises
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.map((exercise, index) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
