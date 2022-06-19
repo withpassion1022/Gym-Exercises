3dbe0ad1e28d737e8ad2c41c958839c352918966
emilkaminski1022 <o.daugherty9se@gmail.com>
2022-06-19 11:42:15 +0430

    add useEffect in ExerciseDetail comp

--- src/Pages/ExerciseDetail.jsx
+++ src/Pages/ExerciseDetail.jsx
@@ -1,21 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Box } from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import Detail from '../components/Detail';
-import ExerciseVideos from '../components/ExerciseVideos';
-import SimilarExercises from '../components/SimilarExercises';
-
-
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const ExerciseDetail = () => {
-  return ( 
-    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
-    <Detail />
-    <ExerciseVideos  />
-    <SimilarExercises  />
-  </Box>
-  )
-}
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
 
-export default ExerciseDetail
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
