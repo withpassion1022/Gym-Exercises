54769dfdb9d00c1236779afe33ec4f28a427ecf4
emilkaminski1022 <o.daugherty9se@gmail.com>
2022-06-19 11:35:34 +0430

    create Detail+ExerciseVideos+SimilarExercises comps and add to ExerciseDetail page

--- src/Pages/ExerciseDetail.jsx
+++ src/Pages/ExerciseDetail.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
+
 
 const ExerciseDetail = () => {
   return ( 
-    <div>ExerciseDetail</div>
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+    <Detail />
+    <ExerciseVideos  />
+    <SimilarExercises  />
+  </Box>
   )
 }
 
--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
 import { Box, Stack, Typography } from "@mui/material";
 import ExerciseCard from "./ExerciseCard";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -18,6 +19,28 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     setCurrentPage(value);
     window.scrollTo({ top: 1800, behavior: "smooth" });
   };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions
+        );
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
 
   return (
     <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
